62f04203676f2c7c50cb5967dfc17f933026086b
TheGreed <160795270+TheGreed@users.noreply.github.com>
2024-03-18 22:03:06 -0300

    Joao pescado
    Algoritimo para calcular multa pelo excesso de peso segundo regulamento de pesca do estado de São Paulo( ficticio ) em C++

---
+++ joao-pescador/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <locale>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+	setlocale (LC_ALL,"portuguese");
+	float kg;
+	float ex;
+	float mul;
+	
+	ex = 0;
+	mul = 0;
+	
+	cout<<"Digite o peso do peixe em Quilo: ";
+	cin>> kg;
+	
+	if(kg >=50){
+		ex = kg-50;
+		mul = ex*4.00;
+		
+		cout <<"O valor da multa é: "<< mul <<" devido ao excesso de peso, que foi: "<< ex;
+	}
+
+	else {
+		cout <<"A pesagem não ultrapassou o excesso.";
+	}
+	
+	
+	
+	return 0;
+}
